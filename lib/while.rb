713421dcbad63936abcb9876581cafd827b821e2
Bhavna C <bc2412@columbia.edu>
2019-01-16 23:21:08 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
 	levitation_force = 6
-	while num_of_hotdogs_eaten < 7
+	while levitation_force < 10
   num_of_hotdogs_eaten += 1
   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
 end
